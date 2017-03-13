cadd126f0f9ff93e989e962b022ab628fff48982
SfTd <j.zolcik@allblue.pl>
2017-03-13 16:48:14 +0100

    Printer upgrade and Technologic printers workaround added.

--- allblue/src/main/java/pl/allblue/bluetooth/BluetoothPrinter.java
+++ allblue/src/main/java/pl/allblue/bluetooth/BluetoothPrinter.java
@@ -1,24 +1,46 @@
 package pl.allblue.bluetooth;
 
+import android.app.Application;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.util.Log;
 
 import org.json.JSONArray;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * Created by SfTd on 02/05/2016.
  */
 public class BluetoothPrinter
 {
+
+    static private byte[] GetBytes_Combine(byte[][] byte_parts)
+    {
+        int length = 0;
+        for (int i = 0; i < byte_parts.length; i++)
+            length += byte_parts[i].length;
+
+        byte[] combined_bytes = new byte[length];
+
+        int current_index = 0;
+        for (int i = 0; i < byte_parts.length; i++) {
+            System.arraycopy(byte_parts[i], 0, combined_bytes, current_index, byte_parts[i].length);
+            current_index += byte_parts[i].length;
+        }
+
+        return combined_bytes;
+    }
 
     static private byte[] GetBytes_Image(Bitmap bitmap, int width)
     {
@@ -29,23 +51,22 @@ public class BluetoothPrinter
         int height = (int)(bitmap.getHeight() * f);
 
         Bitmap scaled_bitmap = bitmap.createScaledBitmap(bitmap, width, height, false);
+        int[] bitmap_pixels = new int[width * height];
+        scaled_bitmap.getPixels(bitmap_pixels, 0, width, 0, 0, width, height);
 
         byte[] image = new byte[8 + (width / 8) * height];
 
         /* Print Format */
         image[0] = 0x1d;
         image[1] = 0x76;
-        image[2] = 48;
+        image[2] = 0x30;
         image[3] = 0;
-        image[4] = (byte)(width / 8);
-        image[5] = 0;
+        image[4] = (byte)((width / 8) % 256);
+        image[5] = (byte)((width / 8) / 256);
         image[6] = (byte)(height % 256);
         image[7] = (byte)(height / 256);
 
-        int[] bm_pixels = new int[width * height];
-        scaled_bitmap.getPixels(bm_pixels, 0, width, 0, 0, width, height);
-
-        /* Image */
+        /* Image Data */
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < (width / 8); j++) {
                 byte color_byte = 0;
@@ -54,13 +75,18 @@ public class BluetoothPrinter
                     int bmp_x = i;
                     int bmp_y = j * 8 + k;
 
-                    if (Color.red(bm_pixels[bmp_x * width + bmp_y]) < 128) {
+                    if (Color.red(bitmap_pixels[bmp_x * width + bmp_y]) < 128)
                         color_byte |= (byte)(1 << (7 - k));
-                    }
                 }
 
                 image[8 + i * (width / 8) + j] = color_byte;
             }
+        }
+
+        /* Technologic printers bug workaround. */
+        for (int i = 8; i < image.length - 3; i++) {
+            if (image[i] == 16 && image[i + 1] == 4 && image[i + 2] == 1)
+                image[i + 2] = 0;
         }
 
         return image;
@@ -79,7 +105,6 @@ public class BluetoothPrinter
         line[4] = 0x0D;
         line[5] = 0x0D;
         line[6] = 0x0D;
-
 
         return line;
     }
@@ -158,24 +183,27 @@ public class BluetoothPrinter
         if (!this.isConnected())
             throw new AssertionError("Printer not connected.");
 
-        BufferedOutputStream socket_os = new BufferedOutputStream(
-                this.socket.getOutputStream());
-
         byte[] line_bytes = BluetoothPrinter.GetBytes_Line();
         byte[] image_bytes = BluetoothPrinter.GetBytes_Image(image, width);
 
-        JSONArray ja = new JSONArray();
-        for (int i = 0; i < image_bytes.length; i++)
-            ja.put(image_bytes[i]);
-        Log.d("BluetoothPrinter", "Length: " + image_bytes.length);
+        byte[] all_bytes = BluetoothPrinter.GetBytes_Combine(new byte[][] {
+                line_bytes, line_bytes, line_bytes,
+                image_bytes,
+                line_bytes, line_bytes, line_bytes
+        });
 
-        for (int i = 0; i < 3; i++)
-            socket_os.write(line_bytes);
-        socket_os.write(image_bytes);
+        this.sendBytes(all_bytes);
+    }
 
-        for (int i = 0; i < 3; i++)
-            socket_os.write(line_bytes);
+    public void sendBytes(byte[] bytes) throws IOException
+    {
+        if (!this.isConnected())
+            throw new AssertionError("Printer not connected.");
 
+        BufferedOutputStream socket_os = new BufferedOutputStream(
+                this.socket.getOutputStream());
+
+        socket_os.write(bytes);
         socket_os.flush();
     }
 
